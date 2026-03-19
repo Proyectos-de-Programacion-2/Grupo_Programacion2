2eade3a65ea85ec6ba7d6161f2fb6ab10035b300
shura99-99 <76755377+shura99-99@users.noreply.github.com>
2026-03-18 22:48:04 -0300

    Update ejercicio1.py

--- labs/lab1/ejercicio1.py
+++ labs/lab1/ejercicio1.py
@@ -1,1 +1,11 @@
-
+# Ejercicio 1 - Ley de Ohm y Alerta de Potencia
+V=float(input("Ingrese valor de Voltaje (V) "))
+I=float(input("Ingrese el valor de Corriente (A) "))
+R=V/I
+P=V*I
+print("El valor de la resistencia es:", (R), "Ohms") 
+print("El valor de la potencia disipada es:", (P), "Watts")
+if P>1000:
+    print("¡PELIGRO! Alta disipacion de potencia disipada detectada.")
+else:
+    print("Operacion en rangos seguros.")
