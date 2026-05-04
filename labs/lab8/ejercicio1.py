7f645b7c462895d505bf68c126e2d0f23e65346e
shura99-99 <76755377+shura99-99@users.noreply.github.com>
2026-05-04 11:34:33 -0400

    Update ejercicio1.py

--- labs/lab8/ejercicio1.py
+++ labs/lab8/ejercicio1.py
@@ -1,1 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+t = np.linspace(0, 10, 100)
+
+x = np.sin(t)
+
+plt.plot(t, x, color='blue')
+plt.title("Lectura de Sensor (Señal Senoidal)")
+plt.xlabel("Tiempo (s)")
+plt.ylabel("Amplitud")
+plt.grid(True)
+plt.show()
