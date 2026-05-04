7ea495af94b539889fd9c2cc56d18e2879b30ab0
shura99-99 <76755377+shura99-99@users.noreply.github.com>
2026-05-04 11:56:59 -0400

    Add pandas DataFrame to manage stock data

--- labs/lab8/ejercicio2.py
+++ labs/lab8/ejercicio2.py
@@ -1,1 +1,9 @@
+import pandas as pd
+datos = {
+    " Componente ": [" Arduino ", " Resistencia 1k", " Capacitor ", " MotorDC"] ,
+    " Stock ": [5 , 500 , 120 , 3]
+}
+df = pd.DataFrame(datos)
+df_maximo = df[df[" Stock "] <10]
+print(df_maximo)
 
