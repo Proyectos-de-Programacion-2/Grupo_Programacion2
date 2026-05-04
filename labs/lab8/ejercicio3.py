fc00765b8a8a92c24798cc720924c2b5fb30ae4f
shura99-99 <76755377+shura99-99@users.noreply.github.com>
2026-05-04 11:59:31 -0400

    Update ejercicio3.py

--- labs/lab8/ejercicio3.py
+++ labs/lab8/ejercicio3.py
@@ -1,1 +1,25 @@
+class Bateria:
 
+    def __init__(self, marca):
+      
+        self.marca = marca
+        self.__carga = 100
+
+    def usar_bateria(self, gasto):
+        new = self.__carga - gasto
+        if new<=0:
+            self.__carga = 0
+            print("¡Bateria Agotada!")
+        else:
+            self.__carga = new
+    
+
+    def ver_carga(self):
+       return(print(self.__carga))
+    
+mi_bateria = Bateria("Duracell")
+
+mi_bateria.ver_carga()
+mi_bateria.usar_bateria(40)
+mi_bateria.ver_carga()
+mi_bateria.usar_bateria(80)
