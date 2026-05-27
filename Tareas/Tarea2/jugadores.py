fa1b87b40dc80109e16987fd5fec2d76b565b373
Javier Huerta <javier.huerta.c@mail.pucv.cl>
2026-05-27 04:03:51 -0400

    Script jugadores y inicio de main

---
+++ Tareas/Tarea2/jugadores.py
@@ -0,0 +1,89 @@
+# Clase Padre
+class jugador:
+
+    def __init__(self, nombre, edad, altura, dorsal):
+
+        self.nombre = nombre
+        self.edad = edad
+        self.altura = altura
+        self.dorsal = dorsal
+
+    def correr(self):
+        print(f"{self.nombre} esta corriendo.")
+
+    def mostrar_rol(self):
+        print("Soy un jugador de la seleccion de Portuguesa.")
+
+    def celebrar(self):
+        print(f"{self.nombre} celebra un gol.")
+    
+    def reclamo(self):
+        print(f"{self.nombre} se acerca al arbitro y le reclama el penal cobrado.")
+
+# Clase hija 1    
+class portero(jugador):
+
+    def __init__(self, nombre, edad, altura, dorsal, atajadas, apariciones):
+        super().__init__(nombre, edad, altura, dorsal)
+        self.atajadas = atajadas
+        self.apariciones = apariciones
+
+    def atajar(self):
+        print(f"{self.nombre} ataja el tiro del rival.")
+
+    def saque_de_arco(self):
+        print(f"{self.nombre} realiza un saque de arco.")
+
+    def mostrar_rol(self):
+        print("Portero")
+
+# Clase hija 2
+class defensa(jugador):
+
+    def __init__(self, nombre, edad, altura, dorsal, balones_recuperados, despejes):
+        super().__init__(nombre, edad, altura, dorsal)
+        self.balones_recuperados = balones_recuperados
+        self.despejes = despejes
+
+    def marcar(self):
+        print(f"{self.nombre} está marcando al delantero rival.")
+
+    def barrida(self):
+        print(f"{self.nombre} realiza una barrida al rival.")
+
+    def mostrar_rol(self):
+        print("Defensa")
+
+# Clase hija 3
+class mediocampista(jugador):
+
+    def __init__(self, nombre, edad, altura, dorsal, asistencias, pases_efectivos):
+        super().__init__(nombre, edad, altura, dorsal)
+        self.asistencias = asistencias
+        self.pases_efectivos = pases_efectivos
+
+    def dar_pase(self):
+        print(f"{self.nombre} da un pase filtrado perfecto.")
+
+    def posesion(self):
+        print(f"{self.nombre} asegura la posesion del balon.")
+
+    def mostrar_rol(self):
+        print("Mediocampista")
+
+#Clase hija 4
+class delantero(jugador):
+
+    def __init__(self, nombre, edad, altura, dorsal, goles, tiros_al_arco):
+        super().__init__(nombre, edad, altura, dorsal)
+        self.goles = goles
+        self.tiros_al_arco = tiros_al_arco
+
+    def patear_al_arco(self):
+        print(f"{self.nombre} patea el balon hacia el arco rival.")
+
+    def cabecear(self):
+        print(f"{self.nombre} salta al balon elevado y cabecea.")
+
+    def mostrar_rol(self):
+        print("Delantero")
--- Tareas/Tarea2/main.py
+++ Tareas/Tarea2/main.py
@@ -1,1 +1,22 @@
+import pandas as pd
+import os
+from jugadores import jugador, portero, defensa, mediocampista, delantero
 
+def main():
+    
+    pais = "Portugal"
+
+    # Lista jugadores (datos de temporada 2025-2026, otros inventados por falta de info)
+    jugadores_titulares = [
+        portero("Diogo Costa", 26, 1.86, 1, atajadas=104, apariciones=49),
+        defensa("João Cancelo", 31, 1.82, 20, balones_recuperados=203, despejes=178),
+        defensa("Rúben Diaz", 29, 1.87, 3, balones_recuperados=189, despejes=140),
+        defensa("Gonçalo Inácio", 24, 1.86, 14, balones_recuperados=147, despejes=98),
+        defensa("Nuno Mendez", 23, 1.80, 25, balones_recuperados=195, despejes=176),
+        mediocampista("João Neves", 21, 1,74, 15, asistencias=8, pases_efectivos=950),
+        mediocampista("Vitinha", 26, 1.72, 23, asistencias=7, pases_efectivos=2572),
+        mediocampista("Bruno Fernandes", 31, 1.79, 8, asistencias=21, pases_efectivos=1639),
+        mediocampista("Bernardo Silva", 31, 1.73, 10, asistencias=4, pases_efectivos=1989),
+        delantero("Rafael Leão", 26, 1.88, 17, goles=9, tiros_al_arco=45),
+        delantero("Cristiano Ronaldo", 41, 1.87, 7, goles=28, tiros_al_arco=161)
+    ]
